23f9c86acb9728f7904f58df6a2657dd6637c9e9
abel302025@hotmail.com <abel302025@hotmail.com>
2025-01-18 22:15:52 -0600

    agregando datos de crear en el formulario y validando los campos

--- app/Http/Controllers/EspecialidadController.php
+++ app/Http/Controllers/EspecialidadController.php
@@ -29,6 +29,19 @@ class EspecialidadController extends Controller
 
     //METODO STORE(Guardar datos en la BD)
     public function store(Request $request){
+        //VALIDACION DE CAMPOS
+        $rules = [
+            'nombre' => 'required|min:3'
+        ];
+
+        $messages = [
+            'nombre.required' => 'El nombre de la Especialidad es Obligatorio',
+            'nombre.min' => 'El nombre de la Especialidad debe de tener mas de 3 caracteres'
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+
         $especialidad = new Especialidad();
         $especialidad->nombre = $request->get('nombre');
         $especialidad->descripcion = $request->get('descripcion');
--- resources/views/Especialidades/create.blade.php
+++ resources/views/Especialidades/create.blade.php
@@ -16,15 +16,26 @@
         </div>
         <div class="card-body">
             <!--  formulario -->
+            @if($errors->any())
+                @foreach($errors->all() as $error)
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <i class ="fas fa-exclamation-triangle"></i>
+                    <span class="alert-text">{{ $error }}</span>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endforeach
+            @endif
             <form action="{{ route('store') }}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label>Nombre de la Especialidad</label>
-                    <input type="text" name="nombre" class="form-control" placeholder="ingrese el nombre de la especialidad"></input>
+                    <input type="text" name="nombre" class="form-control" value="{{ old('nombre') }}" placeholder="ingrese el nombre de la especialidad" required></input>
                 </div>
                 <div class="form-group">
                     <label>Descripcion</label>
-                    <input type="text" name="descripcion" class="form-control" placeholder="ingrese una descripcion"></input>
+                    <input type="text" name="descripcion" class="form-control" value="{{ old('descripcion') }}" placeholder="ingrese una descripcion"></input>
                 </div>
                 <button type="submit" class="btn btn-sm btn-primary">Guardar Especialidad</button>
             </form>
